0648598de671ea528600661ee457b2c7f9e96a62
javeria-farooq <gettingeducation6@gmail.com>
2024-01-13 20:58:59 +0500

    2nd JS Assignment

---
+++ app.js
@@ -0,0 +1,26 @@
+// let firstName = ("Javeria ")
+// let middleName = ("Mohammad ")
+// let lastName = ("Farooq")
+// document.write (firstName + middleName + lastName)
+
+// let name = 'Afnan'
+// let age = 10
+// let fatherName = 'M.Farooq'
+// let roll_no = 12345
+// document.write(
+//     '<table border="3px">' +
+//         '<tr>' +
+//         '<th>' + "Name" + '</th>' +
+//         '<th>' + "Age" + '</th>' + 
+//         '<th>' + "Father" + "Name" + '</th>' +
+//         '<th>' + "Roll" + "No" + '</th>' +
+//         '</tr>' +
+
+//         '<tr>' +
+//            '<td>' + name + '</td>' +
+//            '<td>' + age + '</td>' + 
+//            '<td>' + fatherName + '</td>' +
+//            '<td>' + roll_no + '</td>' +
+//         '</tr>' +
+//     '</table>'
+//     )
